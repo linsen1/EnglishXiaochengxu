33d66abd74401d12b74c0c58d4a7c62b4e83f44c
linsen <linsen@live.cn>
2017-12-19 23:42:56 +0800

    增加历史菜单

---
+++ pages/index/index.js
@@ -0,0 +1,38 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+
+Page({
+  data: {
+    pic: '',
+    chineseWord: '',
+    englishWord: '',
+    xiaobian: ''
+  },
+  onLoad: function () {
+    var that = this;
+    wx.request({
+      url: 'https://api.hlppt.com/api/new/', //仅为示例，并非真实的接口地址
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        that.setData({
+          pic: res.data.pic,
+          chineseWord: res.data.chineseWord,
+          englishWord: res.data.englishWord,
+          xiaobian: res.data.xiaobian
+        })
+        console.log(res.data.pic)
+      }
+    })
+    
+  },
+  onShareAppMessage: function (res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+  }
+})
